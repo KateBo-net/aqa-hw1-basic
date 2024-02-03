3fffaf8f665ae2f9fae30e2671d5972d41eeb416
Kate Bo <bodrakea@gmail.com>
2024-02-03 19:34:15 +0300

    add tests TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void shouldCalculateAmountLessBoundary(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateAmountEqualBoundary(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateAmountMoreBoundary(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual,expected);
+    }
+
+}
